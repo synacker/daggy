04b4d072f636163c7015b4987baa69c4eed6e08c
synacker <milovidovmikhail@gmail.com>
2020-03-30 00:22:04 -0700

    Fixed windows build

--- git_version.py
+++ git_version.py
@@ -24,13 +24,17 @@ SOFTWARE.
 
 import os
 
-def version():
-    stream = os.popen("git describe --match '[0-9]*.[0-9]*.[0-9]*' --abbrev=0 --tags")
-    return stream.read().strip()
+class GitVersion():
+    def __init__(self):
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-def build_number():
-    stream = os.popen("git rev-list {}.. --count".format(version()))
-    return stream.read().strip()
+    def version(self):
+        stream = os.popen("git describe --match [0-9]*.[0-9]*.[0-9] --abbrev=0 --tags")
+        return stream.read().strip()
 
-def full_version():
-    return "{}.{}".format(version(), build_number())
+    def build_number(self):
+        stream = os.popen("git rev-list {}.. --count".format(self.version()))
+        return stream.read().strip()
+
+    def full_version(self):
+        return "{}.{}".format(self.version(), self.build_number())
