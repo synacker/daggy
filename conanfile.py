aff14465552e7f76e491b5199e584d8cb04b4284
synacker <milovidovmikhail@gmail.com>
2021-04-10 22:25:12 +0300

    Started with tests

--- conanfile.py
+++ conanfile.py
@@ -52,11 +52,8 @@ class DaggyConan(ConanFile):
     def set_version(self):
         self.version = GitVersion().full_version()
 
-    def build_requirements(self):
-        self.requires("cmake/3.20.0")
-
     def requirements(self):
-        self.requires("openssl/1.1.1j")
+        self.requires("openssl/1.1.1i", override=True)
         self.requires("qt/6.0.1@bincrafters/stable")
         self.requires("kainjow-mustache/4.1")
 
---
+++ src/DaggyCore/tests/local/DaggyCoreLocalTests.h
@@ -0,0 +1,65 @@
+/*
+MIT License
+
+Copyright (c) 2021 Mikhail Milovidov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include <QtTest>
+
+class DaggyCoreLocalTests : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DaggyCoreLocalTests(QObject *parent = nullptr);
+
+private:
+    bool myCondition()
+    {
+        return true;
+    }
+
+private slots:
+    void initTestCase()
+    {
+        qDebug("Called before everything else.");
+    }
+
+    void myFirstTest()
+    {
+        QVERIFY(true); // check that a condition is satisfied
+        QCOMPARE(1, 1); // compare two values
+    }
+
+    void mySecondTest()
+    {
+        QVERIFY(myCondition());
+        QVERIFY(1 != 2);
+    }
+
+    void cleanupTestCase()
+    {
+        qDebug("Called after myFirstTest and mySecondTest.");
+    }
+
+};
+
