1d517229d8fe63e124687faf5f6406a43382ba7b
synacker <synacker@yandex.ru>
2022-07-25 23:01:05 +0300

    Fix

--- src/cmake/rpath_bin.cmake
+++ src/cmake/rpath_bin.cmake
@@ -1,6 +1,6 @@
 if(CONAN_BUILD)
     if(APPLE)
-        set_target_properties(${TARGET} PROPERTIES INSTALL_NAME_DIR "@executable_path/../${CMAKE_INSTALL_LIBDIR};@executable_path/../${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}")
+        set_target_properties(${TARGET} PROPERTIES INSTALL_RPATH "@executable_path/../${CMAKE_INSTALL_LIBDIR};@executable_path/../${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}")
     elseif(UNIX)
         set_target_properties(${TARGET} PROPERTIES INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}:$ORIGIN/../${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}")
     endif()
