b6d49b2079c7fce5e8edad5c328addd5afab1a07
synacker <synacker@yandex.ru>
2023-10-28 15:39:28 +0300

    Continue with refactoring

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -1,10 +1,4 @@
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-
-conan_basic_setup()
-if(WIN32)
-    conan_set_vs_runtime()
-endif()
-conan_check_compiler()
+include(${CMAKE_BINARY_DIR}/conantoolchain.cmake)
 
 include_directories(${CONAN_INCLUDE_DIRS})
 link_directories(${CONAN_LIBS})
--- conanfile.py
+++ conanfile.py
@@ -69,36 +69,35 @@ class DaggyConan(ConanFile):
             del self.options.fPIC
     
     def build_requirements(self):
-        self.tool_requires("cmake/[>=3.27.7]")
-        self.tool_requires("gtest/[>=1.13.0]")
+        self.tool_requires("cmake/3.27.7")
+        self.tool_requires("gtest/1.13.0")
 
     def requirements(self):
-        self.requires("qt/[>=6.6.0]")
-        self.requires("kainjow-mustache/[>=4.1]")
+        self.requires("qt/6.6.0")
+        self.requires("kainjow-mustache/4.1")
 
         if self.options.with_yaml:
-            self.requires("yaml-cpp/[>=0.8.0]")
+            self.requires("yaml-cpp/0.8.0")
 
         if self.options.with_ssh2:
-            self.requires("libssh2/[>=1.11.0]")
+            self.requires("libssh2/1.11.0")
 
     def layout(self):
-        self.folders.source = "."
+        self.folders.source = "src"
         self.folders.build = "."
         self.folders.generators = self.folders.build
         self.folders.imports = self.folders.build
 
-        self.cpp.libdirs = [self._libdir()]
-        self.cpp.bindir = ["bin"]
+        self.cpp.libdirs = ["lib"]
+        self.cpp.bindirs = ["bin"]
 
         self.cpp.includedirs = ["src"]
 
     def generate(self):
-        libdir = os.path.join(self.folders.build, self._libdir(), self.name)
-        bindir = os.path.join(self.folders.build, "bin")
+        libdir = os.path.join(self.folders.build, self.cpp.libdirs[0], self.name)
+        bindir = os.path.join(self.folders.build, self.cpp.bindirs[0])
         for dep in self.dependencies.values():
             if self.settings.os == "Windows":
-                print(dep.name)
                 if dep.cpp_info.bindirs:
                     copy(self, "*.dll", dep.cpp_info.bindirs[0], bindir) 
                 if dep.cpp_info.libdirs:
@@ -112,11 +111,11 @@ class DaggyConan(ConanFile):
 
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_BINARY_DIR"] = self.folders.build
+        tc.variables["CMAKE_INSTALL_LIBDIR"] = self.cpp.libdirs[0]
         tc.variables["SSH2_SUPPORT"] = self.options.with_ssh2
         tc.variables["YAML_SUPPORT"] = self.options.with_yaml
         tc.variables["CONSOLE"] = self.options.with_console
         tc.variables["PACKAGE_DEPS"] = True
-        tc.variables["CMAKE_INSTALL_LIBDIR"] = self._libdir()
         tc.variables["BUILD_TESTING"] = True
         tc.variables["CONAN_BUILD"] = True
         
@@ -124,15 +123,7 @@ class DaggyConan(ConanFile):
             tc.variables["CMAKE_C_VISIBILITY_PRESET"] = "hidden"
             tc.variables["CMAKE_CXX_VISIBILITY_PRESET"] = "hidden"
             tc.variables["CMAKE_VISIBILITY_INLINES_HIDDEN"] = 1
-        tc.generate()
-
-    def _libdir(self):
-        result = "lib"
-        if self.settings.arch == "x86_64" and self.settings.os == "Linux":
-            result = "lib64"
-        return result
-
-    
+        tc.generate()    
 
     def build(self):
         cmake = CMake(self)
